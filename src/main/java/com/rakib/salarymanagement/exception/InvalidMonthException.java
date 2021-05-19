3ee435818fe5b70ce3d176f86c11b3ffe3a19fbe
rakibccj <rakibccj@gmail.com>
2021-05-19 11:26:28 +0600

    added dateUtil

---
+++ src/main/java/com/rakib/salarymanagement/exception/InvalidMonthException.java
@@ -0,0 +1,11 @@
+package com.rakib.salarymanagement.exception;
+
+public class InvalidMonthException extends Exception {
+    public InvalidMonthException() {
+        super();
+    }
+
+    public InvalidMonthException(String message) {
+        super(message);
+    }
+}
